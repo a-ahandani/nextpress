7db3424caae9651a66693cf1c2c7fda9529c62db
Ahmad Ahandani <a.e.ahandani@gmail.com>
2023-06-07 01:00:57 -0700

    update ui

--- components/tags.tsx
+++ components/tags.tsx
@@ -1,6 +1,6 @@
 export default function Tags({ tags }) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-4xl mx-auto">
       <p className="mt-8 text-lg font-bold">
         Tagged
         {tags.edges.map((tag, index) => (
