d3df546c28076000c8133687ea30e15aee79b9ff
Ahmad <a.e.ahandani@gmail.com>
2023-06-07 16:18:30 -0700

    add icons library

--- components/date.tsx
+++ components/date.tsx
@@ -1,9 +1,14 @@
 import { parseISO, format } from "date-fns";
+import { CalendarDaysIcon } from "@heroicons/react/24/solid";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
   return (
-    <time dateTime={dateString} className="font-sans to-blue-50">
+    <time
+      dateTime={dateString}
+      className="font-sans to-blue-50 flex items-center"
+    >
+      <CalendarDaysIcon className="h-5 w-5 mr-2 " />{" "}
       {format(date, "LLLL	d, yyyy")}
     </time>
   );
