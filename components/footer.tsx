c06d6df6cf9129b90bb09af4358122bdba46e21c
Ahmad <a.e.ahandani@gmail.com>
2023-06-07 10:00:39 -0700

    update fonts style

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center"></div>
       </Container>
