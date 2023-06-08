4819aa17349e326f4ab05a1a25ad3be4867a76f9
Ahmad Ahandani <a.e.ahandani@gmail.com>
2023-06-07 19:16:09 -0700

    add gtm

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,24 @@
+import Script from 'next/script'
+const gtm = process.env.GA_MEASUREMENT_ID
+
 export default function Container({ children }) {
-  return <div className="container mx-auto px-5">{children}</div>;
+
+
+  return <div className="container mx-auto px-5">
+
+    <Script
+      src={"https://www.googletagmanager.com/gtag/js?id=" + gtm}
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${gtm}');
+    `}
+    </Script>
+
+    {children}</div>;
 }
