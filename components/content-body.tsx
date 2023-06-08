a7f8dda7bd6eb22166117fdb17cdff7caa359ef7
Ahmad <a.e.ahandani@gmail.com>
2023-06-08 14:51:38 -0700

    improve ui

--- components/content-body.tsx
+++ components/content-body.tsx
@@ -2,7 +2,7 @@ import styles from "./content-body.module.css";
 
 export default function ContentBody({ content }) {
   return (
-    <div className="font-sans max-w-4xl mx-auto">
+    <div className="font-sans  mx-auto">
       <div
         className={styles.content}
         dangerouslySetInnerHTML={{ __html: content }}
