10f2dfe7ed4d999583dfced0cde410d2379f5d4d
Ahmad <a.e.ahandani@gmail.com>
2023-06-12 06:28:20 -0700

    update ui

--- components/content-title.tsx
+++ components/content-title.tsx
@@ -1,7 +1,7 @@
 export default function ContentTitle({ children }) {
   return (
     <h1
-      className="font-serif text-2xl md:text-5xl sm:text-3xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 "
+      className="text-3xl mb-3 leading-snug font-serif "
       dangerouslySetInnerHTML={{ __html: children }}
     />
   );
