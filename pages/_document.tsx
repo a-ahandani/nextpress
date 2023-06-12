8a520599033f3b3dbf35d63cbc3917d5dfd12827
Ahmad Ahandani <a.e.ahandani@gmail.com>
2023-06-11 21:22:37 -0700

    add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,28 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-
-
-        <title>Ahamd E. Ahandani</title>
-        <meta name="title" content="Ahamd E. Ahandani" />
-        <meta name="description" content="Software developer." />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ahandani.com/" />
-        <meta property="og:title" content="Ahamd E. Ahandani" />
-        <meta property="og:description" content="Software developer." />
-        <meta property="og:image" content="https://ahandani.com/meta.png" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://ahandani.com/" />
-        <meta property="twitter:title" content="Ahamd E. Ahandani" />
-        <meta property="twitter:description" content="Software developer." />
-        <meta property="twitter:image" content="https://ahandani.com/meta.png" />
-
-
-
-      </Head>
+      <Head />
       <body className=" bg-accent-1 " >
         <Main />
         <NextScript />
